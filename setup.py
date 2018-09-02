5046a431eef3eb9509410636bf2de44ee4acab6a
Marc Ford <mrfxyz567@gmail.com>
2018-09-02 09:26:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup
 
+with open('README.md', "r") as fh:
+    long_description = fh.read()
+
 setup_args = dict(
     name='aionewsapi',
     version='1.1.0',
     author='Marc Ford',
     url='https://github.com/mfdeux/aionewsapi',
     description='Asyncio client for interacting with newsapi.org',
-    long_description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     packages=['aionewsapi'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        "Operating System :: OS Independent",
     ],
     install_requires=[
         'aiohttp',
